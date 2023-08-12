b238eee9c4bf6dbc6bca90a81cd8df3f7b8db88b
koray demir <koraydemirmc@gmail.com>
2023-08-13 00:13:25 +0300

    kod iyileştirmesi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,53 @@
 const inputBox = document.querySelector("#input-box");
 const taskContainer = document.querySelector(".todo-list");
-const ekleyici = document.querySelector("#eklebtn");
-const filtreleme = document.querySelectorAll('[name="filter"]');
+const filters = document.querySelectorAll('[name="filter"]');
 const leftItem = document.querySelector(".footer-leftitem");
 const rightItem = document.querySelector(".footer-rightitem");
+const completeAll = document.querySelector(".completeAll");
+const addForm = document.querySelector(".add-form");
 
 const LeftTask = () => {
-  const itemLeft = [...taskContainer.children].filter(item => item.className === "task").length;
-  leftItem.textContent = `${itemLeft} Item Left`;
+  const itemLeft = document.querySelectorAll("li.task:not(.completed)").length;
+  leftItem.innerText = itemLeft > 1 ? itemLeft + ' items left' : itemLeft + ' item left';
 };
-
 
 function clearCompleted() {
-  const completedItems = Array.from(taskContainer.querySelectorAll('.completed'));
-  if (completedItems.length > 0) {
-    rightItem.textContent = "Clear Completed";
-    rightItem.addEventListener("click", () => {
-      for (const item of completedItems) {
-        item.remove();
-      }
-      rightItem.textContent = "";
-      LeftTask();
-      saveData();
-    });
-  } else {
-    rightItem.textContent = "";
-    rightItem.removeEventListener("click", null);
-  }
+  const completedItems = [...taskContainer.querySelectorAll('.completed')];
+  completedItems.forEach(element => {
+    element.remove();
+  });
 }
+rightItem.addEventListener('click', clearCompleted);
+  
+const addTask = (e) => {
+  e.preventDefault();
 
-const addTask = () => {
   if (inputBox.value === "") {
     inputBox.classList.add("warn");
-    inputBox.focus();
-  } else {
-    const taskHTML = `
-      <li class="task">${inputBox.value}
-        <span class="sil">×</span>
-      </li>
-    `;
-    inputBox.value = "";
-    inputBox.focus();
-    taskContainer.insertAdjacentHTML("beforeend", taskHTML);
-    inputBox.classList.remove("warn");
-    saveData();
-    LeftTask();
   }
+  
+  const taskHTML = `
+  <li class="task" draggable="true">${inputBox.value}
+    <span class="sil">×</span>
+  </li>`
+
+  addForm.reset();
+  taskContainer.insertAdjacentHTML("beforeend", taskHTML);
+  inputBox.classList.remove("warn");
+  systemFunction()
+
+  if([...taskContainer.children].length >= 7){
+    taskContainer.id = "scroll"
+  } else {taskContainer.id = ""}
+
+  AddEventListenerDrag()
 };
+addForm.addEventListener('submit', addTask)
 
-inputBox.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    addTask();
-  }
-});
 
-taskContainer.addEventListener("dblclick", (t) => {
-  if (t.target.tagName === "LI") {
-    enableEdit(t.target);
+taskContainer.addEventListener("dblclick", (e) => {
+  if (e.target.tagName === "LI") {
+    enableEdit(e.target);
   }
 });
 
@@ -82,18 +73,17 @@ const enableEdit = (todoItem) => {
 taskContainer.addEventListener("click", (tikla) => {
   if (tikla.target.tagName === "LI") {
     tikla.target.classList.toggle("completed");
-    clearCompleted();
-    saveData();
-    LeftTask();
+    systemFunction()
   } else if (tikla.target.classList.contains("sil")) {
     const parentElement = tikla.target.parentElement;
     parentElement.classList = "deletedtask";
     tikla.target.parentElement.firstChild.textContent = "Deleted Task";
     setTimeout(function() {
+      if([...taskContainer.children].length <= 7){
+        taskContainer.id = ""
+      }
       parentElement.remove();
-      clearCompleted();
-      LeftTask();
-      saveData();
+      systemFunction()
     }, 900);
   }
 });
@@ -102,9 +92,13 @@ const saveData = () => {
   localStorage.setItem("data", taskContainer.innerHTML);
 };
 
-const loadData = () => {
+const loadData = (e) => {
   taskContainer.innerHTML = localStorage.getItem("data");
-  LeftTask();
+  if([...taskContainer.children].length <= 7){
+    taskContainer.id = ""
+  }else {taskContainer.id = "scroll"}
+  AddEventListenerDrag()
+  systemFunction()
 };
 
 for (const filter of document.querySelectorAll('[name="filter"]')) {
@@ -114,3 +108,75 @@ for (const filter of document.querySelectorAll('[name="filter"]')) {
 }
 
 loadData();
+
+// taskContainer.children.addEventListener("dragstart",() => {
+//   console.log("çalıştı")
+// });
+
+let dragStartIndex;
+
+function AddEventListenerDrag (){
+  const draggable = taskContainer.querySelectorAll("li.task");
+  
+  taskContainer.addEventListener("dragstart", dragStart)
+  
+  draggable.forEach(e => {
+    e.addEventListener("dragleave",dragLeave)
+    e.addEventListener("dragover",dragOver)
+    e.addEventListener("drop",dragDrop)
+    e.addEventListener("dragenter",dragEnter)
+  });
+}
+const liElements = [...taskContainer.querySelectorAll("li.task")];
+function dragStart(event) {
+  
+  dragStartIndex = liElements.indexOf(event.target);
+}
+
+function dragOver(e){
+  e.preventDefault();
+  // console.log("DragOver");this.id = ""
+}
+function dragDrop(event){
+  const dragEndIndex = liElements.indexOf(event.target)
+  swapItems(dragStartIndex,dragEndIndex)
+  event.target.id = ""
+  
+  // console.log("DragDrop");
+}
+function swapItems (fromIndex , toIndex){
+
+  const itemOne = taskContainer.children[fromIndex]
+  const itemTwo = taskContainer.children[toIndex]
+  let makeSwap = taskContainer.children
+
+  makeSwap[fromIndex].appendChild(itemTwo)
+  makeSwap[toIndex].appendChild(itemOne)
+
+
+}
+
+function dragEnter(){
+  this.id = "over"
+}
+function dragLeave() {
+  this.id = ""
+}
+
+completeAll.addEventListener("click", () => {
+  
+  const notCompleted = document.querySelectorAll("li.task:not(.completed)")
+
+  if (notCompleted.length !== 0) {
+    notCompleted.forEach(e => e.classList.add("completed"));
+  } else if(notCompleted.length === 0){
+    document.querySelectorAll("li.task.completed").forEach(e => e.classList.remove("completed"))
+  }
+  systemFunction()
+});
+
+
+function systemFunction() {
+  saveData()
+  LeftTask()
+}
